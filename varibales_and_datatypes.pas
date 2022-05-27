3e6b58d374b783b5512160120a8b82799496b4af
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-27 17:03:45 +0530

    Create varibales_and_datatypes.pas

---
+++ varibales_and_datatypes.pas
@@ -0,0 +1,11 @@
+program varibales_and_datatypes(input, output);
+begin
+
+    // Variables Hold data
+    // These data can be changed during the program's operation
+    // the datatype tells what type of data that the computer is capable to hold
+
+    // integer
+        // Whole number, varies by system
+
+end.
