2730f48cfb4d698688f24445c0aa407d2cf275b7
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-29 19:05:07 +0530

    Update functions.pas

--- functions.pas
+++ functions.pas
@@ -2,7 +2,44 @@
 // begin
                         
 // end;
-program functions(input, output);
-begin
+program example_functions(input, output);
 
+var
+    answer, num1, num2: real;
+    ui: string;
+    error: integer;
+
+// in which both x and y are real
+// use `x: real, y: real` if you want
+function multiplyNumber(x, y: real):real;
+begin
+    // this is the return line
+    // this should be equal to the function name
+    multiplyNumber := x * y;
+end;
+
+procedure quit;
+begin
+    writeln;
+    writeln;
+    write('Press <Enter> to quit');
+    readln;
+end;
+
+begin
+    repeat
+        write('Enter number 1: ');
+        readln(ui);
+        val(ui, num1, error);
+    until (error = 0);
+
+    repeat
+        write('Enter number 2: ');
+        readln(ui);
+        val(ui, num2, error);
+    until (error = 0);
+
+    answer := multiplyNumber(num1, num2);
+    writeln('the answer is: ', answer:0:2); // this is basic real number formatting in pascal, discuessed earlier
+    quit;
 end.
